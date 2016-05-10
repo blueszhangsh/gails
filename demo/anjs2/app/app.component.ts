f92ad1d1b7e22b381aeea8dc0ca11678db61f643
Jitang Zheng <jitang.zheng@gmail.com>
2016-05-09 17:37:46 -0700

    emberjs

--- demo/anjs2/app/app.component.ts
+++ demo/anjs2/app/app.component.ts
@@ -2,6 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App.</h1>'
+    template: '<h1>{{title}}</h1><h2>{{note.content}}详细信息</h2>'
 })
-export class AppComponent { }
+
+export class AppComponent {
+  title = "测试";
+  note: Note = {
+    id:1,
+    content:'笔记1'
+  };
+}
+
+export class Note{
+  id: number;
+  content: string;
+}
